49b80ec2b8b16af32958f275f2a20e5bf46b41bb
Abhishek Taur <abhishektaur123@gmail.com>
2020-02-04 01:19:51 -0600

    Lab07 Done

--- sv_design_verification_v20-3/lab07-mem/mem_test.sv
+++ sv_design_verification_v20-3/lab07-mem/mem_test.sv
@@ -38,40 +38,79 @@ initial
   int error_status;
 
     $display("Clear Memory Test");
-
+// SYSTEMVERILOG: enhanced for loop
     for (int i = 0; i< 32; i++)
-       // Write zero data to every address location
-
+       write_mem (i, 0, debug);
     for (int i = 0; i<32; i++)
       begin 
-       // Read every address location
-
+       read_mem (i, rdata, debug);
        // check each memory location for data = 'h00
-
+       error_status = checkit (i, rdata, 8'h00);
       end
-
-   // print results of test
+// SYSTEMVERILOG: void function
+    printstatus(error_status);
 
     $display("Data = Address Test");
-
+// SYSTEMVERILOG: enhanced for loop
     for (int i = 0; i< 32; i++)
-       // Write data = address to every address location
-       
+       write_mem (i, i, debug);
     for (int i = 0; i<32; i++)
       begin
-       // Read every address location
-
+       read_mem (i, rdata, debug);
        // check each memory location for data = address
-
+       error_status = checkit (i, rdata, i);
       end
-
-   // print results of test
+// SYSTEMVERILOG: void function
+    printstatus(error_status);
 
     $finish;
   end
 
-// add read_mem and write_mem tasks
+// SYSTEMVERILOG: default task input argument values
+task write_mem (input [4:0] waddr, input [7:0] wdata, input debug = 0);
+  @(negedge clk);
+  write <= 1;
+  read  <= 0;
+  addr  <= waddr;
+  data_in  <= wdata;
+  @(negedge clk);
+  write <= 0;
+  if (debug == 1)
+    $display("Write - Address:%d  Data:%h", waddr, wdata);
+endtask
 
-// add result print function
+// SYSTEMVERILOG: default task input argument values
+task read_mem (input [4:0] raddr, output [7:0] rdata, input debug = 0);
+   @(negedge clk);
+   write <= 0;
+   read  <= 1;
+   addr  <= raddr;
+   @(negedge clk);
+   read <= 0;
+   rdata = data_out;
+   if (debug == 1) 
+     $display("Read  - Address:%d  Data:%h", raddr, rdata);
+endtask
+
+function int checkit (input [4:0] address,
+                      input [7:0] actual, expected);
+  static int error_status;   // static variable
+  if (actual !== expected) begin
+    $display("ERROR:  Address:%h  Data:%h  Expected:%h",
+                address, actual, expected);
+// SYSTEMVERILOG: post-increment
+     error_status++;
+   end
+// SYSTEMVERILOG: function return
+   return (error_status);
+endfunction: checkit
+
+// SYSTEMVERILOG: void function
+function void printstatus(input int status);
+if (status == 0)
+   $display("Test Passed - No Errors!");
+else
+   $display("Test Failed with %d Errors", status);
+endfunction
 
 endmodule
---
+++ sv_design_verification_v20-3/lab07-mem/run.f
@@ -0,0 +1,7 @@
+//-----------------------------------------------
+// System Verilog run file for lab07
+//------------------------------------------------
+
+mem.sv  // compile src file
+mem_test.sv            // compile top level module
+top.sv // top file
